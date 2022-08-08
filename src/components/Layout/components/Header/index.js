b79574781c14b46c93cda2fe89a9ae23f534e727
tuananh-2305 <101982859+tuananh-2305@users.noreply.github.com>
2022-08-08 18:06:44 +0700

    Header #4

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import Tippy from '@tippyjs/react/headless';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark, faSpinner, faMagnifyingGlass, faEllipsisVertical, faLanguage, faCircleQuestion, faKeyboard } from '@fortawesome/free-solid-svg-icons';
 
 import Button from '~/components/Button';
 import AccoutItem from '~/components/Accoutitem';
@@ -9,8 +9,25 @@ import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
+import Menu from '~/components/Popper/Menu';
 
 const cx = classNames.bind(styles);
+const MENU_ITEMS=[
+    {
+        icon: <FontAwesomeIcon icon={faLanguage}/>,
+        title: 'English'
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion}/>,
+        title: 'Feed back and help',
+        to:'/feedback'
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard}/>,
+        title: 'Keyboard shortcuts',
+    }
+];
+
 function Header() {
     const [searchResult, setSearchResult] = useState([])
 
@@ -56,6 +73,14 @@ function Header() {
                 <div className={cx('action')}>
                     <Button text >Up Load</Button>
                     <Button primary>Log In</Button>
+                
+                <Menu
+                    items={MENU_ITEMS}
+                >
+                    <button className={cx('more-btn')}>
+                        <FontAwesomeIcon icon={faEllipsisVertical}/>
+                    </button>
+                </Menu>
                 </div>
             </div>
         </header>
