86f58b5289272cf8fda81ee9ae06ce3ac7b87078
tuananh-2305 <101982859+tuananh-2305@users.noreply.github.com>
2022-08-14 10:46:13 +0700

    Logic phần Header #3

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -1,4 +1,5 @@
 import { useEffect, useState, useRef } from 'react';
+import { useDebounce } from '~/hooks';
 import {
     faCircleXmark,
     faSpinner,
@@ -19,17 +20,19 @@ function Search() {
     const [showResult, setShowResult] = useState(true);
     const [loading, setLoading] = useState(false);
 
+    const debounced = useDebounce(searchValue, 500);
+
     const inputRef = useRef()
 
     useEffect(() => {
-        if(!searchValue.trim()){
+        if(!debounced.trim()){
             setSearchResult([])
             return;
         }
 
         setLoading(true)
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
             .then((res) => res.json())
             .then((res) => {
                 setSearchResult(res.data);
@@ -39,7 +42,7 @@ function Search() {
                 setLoading(false);
             })
         
-    }, [searchValue]);
+    }, [debounced]);
     const handleClear = () => {
         setSearchValue('');
         setSearchResult([]);
