8bd2776668378826225d2bfcbe7078912f59dbe9
tuananh-2305 <101982859+tuananh-2305@users.noreply.github.com>
2022-08-16 17:20:36 +0700

    Tối ưu Header

--- src/layouts/components/Search/Search.js
+++ src/layouts/components/Search/Search.js
@@ -15,15 +15,15 @@ const cx = classNames.bind(styles);
 function Search() {
     const [searchValue, setSearchValue] = useState('');
     const [searchResult, setSearchResult] = useState([]);
-    const [showResult, setShowResult] = useState(true);
+    const [showResult, setShowResult] = useState(false);
     const [loading, setLoading] = useState(false);
 
-    const debounced = useDebounce(searchValue, 500);
+    const debouncedValue = useDebounce(searchValue, 500);
 
     const inputRef = useRef();
 
     useEffect(() => {
-        if (!debounced.trim()) {
+        if (!debouncedValue.trim()) {
             setSearchResult([]);
             return;
         }
@@ -31,14 +31,14 @@ function Search() {
         const fetchApi = async () => {
             setLoading(true);
 
-            const result = await searchSevices.search(debounced);
+            const result = await searchSevices.search(debouncedValue);
            
             setSearchResult(result);
             setLoading(false);
         }
         fetchApi()
         
-    }, [debounced]);
+    }, [debouncedValue]);
     const handleClear = () => {
         setSearchValue('');
         setSearchResult([]);
@@ -79,7 +79,7 @@ function Search() {
                     <input
                         ref={inputRef}
                         value={searchValue}
-                        placeholder="Search accouts and videos"
+                        placeholder="Search accounts and videos"
                         spellCheck={false}
                         onChange={handleChange}
                         onFocus={() => setShowResult(true)}
