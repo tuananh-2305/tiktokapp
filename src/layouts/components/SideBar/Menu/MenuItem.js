8acc101e818c0d5976e2fa1973af6dd3023b9d7c
tuananh-2305 <101982859+tuananh-2305@users.noreply.github.com>
2022-08-17 15:06:12 +0700

    UI sidebar #1

--- src/layouts/components/SideBar/Menu/MenuItem.js
+++ src/layouts/components/SideBar/Menu/MenuItem.js
@@ -5,7 +5,7 @@ import styles from './Menu.module.scss';
 
 const cx = classNames.bind(styles);
 
-function Menu({title, to, icon, activeIcon}) {
+function MenuItem({title, to, icon, activeIcon}) {
     return ( 
         <NavLink className={(nav) => cx('menu-item', {active : nav.isActive})} to={to}>
             <span className={cx('icon')}>{icon}</span>
@@ -15,10 +15,11 @@ function Menu({title, to, icon, activeIcon}) {
      );
 }
 
-Menu.propTypes ={
+MenuItem.propTypes ={
     tittle: PropTypes.string.isRequired,
     to: PropTypes.string.isRequired,
     icon: PropTypes.node.isRequired,
+    activeIcon: PropTypes.node.isRequired,
 }
 
-export default Menu;
+export default MenuItem;
